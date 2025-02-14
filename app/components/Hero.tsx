c780565748aef1add95b283a362df8aa46c95011
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-02-14 16:16:26 +0530

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -55,7 +55,7 @@ export default function Hero() {
         
         <div className="flex justify-center mt-8">
           <a
-            href="https://forms.gle/JVTWeKAtjWTFXyFT7"
+            href="https://forms.easebuzz.in/sign-up/Mandsaur0rhTp/Spandan_Hackathon_Tournament_2K25/?inst_name=Mandsaur0rhTp&form_name=Spandan_Hackathon_Tournament_2K25&jsonData=1"
             className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           >
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
