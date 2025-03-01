88a305db70418e76e60583a9d32348772c6ed8e0
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-03-01 11:23:13 +0530

    Update Themes.tsx

--- app/components/Themes.tsx
+++ app/components/Themes.tsx
@@ -6,32 +6,32 @@ export default function Themes() {
     {
       title: "AI/ML/DL",
       description: "Explore the cutting-edge world of AI, Machine Learning, and Deep Learning to innovate and solve real-world challenges.",
-      pdfUrl: "/pdfs/ai-ml-dl.pdf" 
+      pdfUrl: "/aiml.pdf" 
     },
     {
       title: "Open Innovation",
       description: "Unleash the power of open innovation to transform healthcare, finance, edtech, and more with collaborative, cutting-edge solutions.",
-      pdfUrl: "/pdfs/open-innovation.pdf"
+      pdfUrl: "/open.pdf"
     },
     {
       title: "Cyber Security",
       description: "Empower the future of digital safety by innovating and collaborating on cutting-edge cybersecurity solutions to protect against evolving threats.",
-      pdfUrl: "/pdfs/cyber-security.pdf"
+      pdfUrl: "/cyber.pdf"
     },
     {
       title: "Blockchain",
       description: "Revolutionize industries with decentralized solutions and transparent technologies through the power of blockchain innovation.",
-      pdfUrl: "/pdfs/blockchain.pdf"
+      pdfUrl: "/bct.pdf"
     },
     {
       title: "Development",
       description: "Build innovative, scalable, and user-centric web and mobile applications that redefine digital experiences.",
-      pdfUrl: "/pdfs/development.pdf"
+      pdfUrl: "/appweb.pdf"
     },
     {
       title: "IoT",
       description: "Connect the world seamlessly with smart, intuitive solutions through the power of the Internet of Things (IoT).",
-      pdfUrl: "/pdfs/iot.pdf"
+      pdfUrl: "/iot.pdf"
     },
   ];
 
