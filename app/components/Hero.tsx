6ed91115a7553444e12bd554413f68dc9fc528cf
Kavy Porwal <159620541+IamKavy47@users.noreply.github.com>
2025-03-21 11:36:22 +0530

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -61,7 +61,7 @@ export default function Hero() {
           >
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-2 text-lg font-medium text-white backdrop-blur-3xl">
-              Presentation Template
+              Problem Statement
             </span>
           </a>
         </div>
